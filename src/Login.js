a8e6581c80ba7a0887fe83abc07dfcd2ff1e21b7
RS <radhe212221@gmail.com>
2022-05-31 22:05:58 +0530

    added state to login page

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Login() {
+  const state = useSelector(s => s)
+  const [ob, setob] = useState({
+    email: "",
+    password: "",
+    role: ""
+  })
+
+  const handleChange = e => {
+    let { name, value } = e.target
+    setob({ ...ob, [name]: value })
+  }
+  const handlesubmit = () => {
+    let data = state[ob.role]
+    console.log(data)
+  }
   return (
-    <div>Login</div>
+    <div>
+      <h1>login</h1>
+      <input onChange={handleChange} name='email' value={ob.email} placeholder='username' />
+      <input onChange={handleChange} name='password' value={ob.password} placeholder='password' />
+      <select onChange={handleChange} name='role' value={ob.role}>
+        <option value=''>Select a Role</option>
+        <option value="admin">admin</option>
+        <option value="faculty">faculty</option>
+        <option value="student">student</option>
+      </select>
+      <button onClick={handlesubmit}>login</button>
+    </div>
   )
 }
